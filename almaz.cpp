9eca0e47637c239e1e8bec6375ca6d095f579b8c
Cheatozz <40584191+Cheatozz@users.noreply.github.com>
2021-12-02 01:04:56 +0300

    Add files via upload

---
+++ almaz.cpp
@@ -0,0 +1,47 @@
+﻿// almaz.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Shape {
+protected:
+    string name;
+    string type;
+public:
+    Shape(string name, string type){
+        this->name = name;
+        this->type = type;
+    }
+};
+
+class Rectangle: public Shape {
+
+};
+
+class Square: public Rectangle {
+
+};
+
+class Triangle: public Shape {
+
+};
+
+class Circle: public Shape {
+
+};
+
+class Cube: public Square{
+
+};
+
+class Sphere: public Circle {
+
+};
+
+int main()
+{
+   
+}
+
